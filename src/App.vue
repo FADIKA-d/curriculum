2ecb034439f1dfbadd05d3ff8382372098fe5e3c
80010-70-05 <80010-70-05@exchange.ad.afpanet>
2020-06-16 16:31:46 +0200

    cv repo 1

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+<carousel>
+<carousel-slide>
+  <div class="card"></div>
+</carousel-slide>
+<carousel-slide>Expériences</carousel-slide>
+<carousel-slide>Formations</carousel-slide>
+<carousel-slide>Compétences</carousel-slide>
+<carousel-slide>Réalisations</carousel-slide>
+<carousel-slide>Langues</carousel-slide>
+</carousel>
+
+  </div>
+</template>
+
+<script>
+import Carousel from './components/carousel/Carousel'
+import CarouselSlide from './components/carousel/CarouselSlide'
+
+export default {
+  data () {
+   
+  },
+  components: {
+    Carousel,
+    CarouselSlide
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div>
+<slot></slot>
+<button @click.prevent="prevent">Précédent</button>
+<button @click.prevent="next">Suivant</button>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        console.log(this.$children)
+        return {
+            index: 0,
+            slides: []
+        }
+    },
+    mounted() {
+        console.log(this.$children)
+        console.log(this.index)
+        this.slides = this.$children
+        this.slides.forEach((slide, i) => {
+            slide.index = i
+        })
+    },
+    computed: {
+        slidesCount() { 
+            return this.slides.length 
+        }
+    },
+    methods: {
+        next () {
+            this.index ++
+             if (this.index >= this.slidesCount) {
+                 this.index = 0
+             }
+        },
+        prevent () {
+            this.index --
+             if (this.index < 0) {
+                 this.index = this.slidesCount -1
+             }
+        }
+    }
+}
+</script>
+
+<style src="./cv.css"></style>
